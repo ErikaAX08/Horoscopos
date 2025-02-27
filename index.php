be7a98c8ba0b7c416c95daae1621308cdc1e3906
ErikaAX08 <eamastalxochimitl@gmail.com>
2025-02-27 17:06:34 -0600

    Index with routing for authentication

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . "/app/config/autoload.php";
+
+$route = isset($_GET['route']) ? $_GET['route'] : 'home';
+
+switch ($route) {
+    case 'register':
+        require_once __DIR__ . "/app/views/register_view.php";
+        break;
+    case 'login':
+        require_once __DIR__ . "/app/views/login_view.php";
+        break;
+    default:
+        require_once __DIR__ . "/app/views/home_view.php";
+        break;
+}
