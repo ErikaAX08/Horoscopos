a50a32d274d682daf5910177fc1e285b772af33b
Rodrigo Baez Aguilar <rodrigo.baezag@alumno.buap.mx>
2025-02-27 20:21:06 -0600

    Se conecto el login con la base de datos

--- app/config/conexion_bd.php
+++ app/config/conexion_bd.php
@@ -1,4 +1,11 @@
 <?php
-$conexion=new mysqli("localhost","root","","horoscopo");
-$conexion->set_charset("utf8");
+function Conectarse()
+{
+    if(!($link=mysqli_connect("localhost","root","", "horoscopo")))
+    {
+        echo "error conectando a la base de datos";
+        exit();
+    }
+    return $link;
+}
 ?>
