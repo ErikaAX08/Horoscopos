0092c89dfe313b5a85d756d31613b5446f593bc2
ErikaAX08 <eamastalxochimitl@gmail.com>
2025-02-27 18:09:01 -0600

    Register view added

--- app/views/register_view.php
+++ app/views/register_view.php
@@ -1,10 +1,73 @@
 <?php
+$additional_styles = "pages/auth.css";
 include __DIR__ . '/../templates/header.php';
 ?>
 
-<div class="content">
-    <h1>Register Page</h1>
-</div>
+<main class="main">
+    <div class="content">
+        <h2 class="title">Únete al universo de los astros</h2>
+        <p class="description">Crea una cuenta de RIEAV</p>
+        <form class="form" method="POST">
+            <label for="name">Nombre</label>
+            <input class="input" id="name" type="text" name="name" placeholder="¿Cómo te llaman los astros?" required>
+
+            <label for="last_name">Apellido Paterno</label>
+            <input class="input" id="last_name" type="text" name="last_name" placeholder="Apellido de tu constelación" required>
+
+            <label for="mother_last_name">Apellido Materno</label>
+            <input class="input" id="mother_last_name" type="text" name="mother_last_name" placeholder="Tu otro vínculo estelar" required>
+
+            <label for="nick">Nick</label>
+            <input class="input" id="nick" type="text" name="nick" placeholder="El alias de tu destino" required>
+
+            <label for="email">Correo</label>
+            <input class="input" id="email" type="email" name="email" placeholder="Ingresa tu correo estelar" required>
+
+            <label for="password">Contraseña</label>
+            <input class="input" id="password" type="password" name="password" placeholder="Tu llave cósmica" required>
+
+            <label for="birthdate">Fecha de nacimiento</label>
+            <input class="input" id="birthdate" type="date" name="birthdate" placeholder="Hora, día, mes y año de tu conexión astral" required>
+
+            <label for="gender">Género</label>
+            <select class="input dropdown" id="gender" name="gender">
+                <option value="identity_cosmic">Identidad cósmica</option>
+                <option value="hombre">Hombre</option>
+                <option value="mujer">Mujer</option>
+                <option value="no_especifico">Deseo no especificar</option>
+            </select>
+
+            <label for="partner_birthdate">Fecha de nacimiento de tu pareja (Opcional)</label>
+            <input class="input" id="partner_birthdate" type="date" name="partner_birthdate" placeholder="Hora, día, mes y año del instante en que su destino comenzó">
+
+            <label for="illnesses">Enfermedades que has tenido en el año</label>
+            <input class="input" id="illnesses" type="text" name="illnesses" placeholder="Eventos que han afectado tu energía vital">
+
+            <div class="row">
+                <div class="col">
+                    <select class="input dropdown" id="abundance" name="abundance">
+                        <option value="flow">¿Cómo fluye tu abundancia?</option>
+                        <option value="buena">Buena</option>
+                        <option value="mala">Mala</option>
+                        <option value="regular">Regular</option>
+                    </select>
+                </div>
+
+                <div class="col">
+                    <select class="input dropdown" id="work_energy" name="work_energy">
+                        <option value="energia">Energía actual en tu trabajo</option>
+                        <option value="desempleado">Desempleado</option>
+                        <option value="empleado">Empleado</option>
+                        <option value="empresario">Empresario</option>
+                    </select>
+                </div>
+            </div>
+
+            <button class="main_button" type="submit">Crear Cuenta</button>
+        </form>
+        <p class="small_text">¿Ya tienes cuenta? <a href="#">Inicia sesión</a></p>
+    </div>
+</main>
 
 <?php
 include __DIR__ . '/../templates/footer.php';
