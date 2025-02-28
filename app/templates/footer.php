66021fdbb75fd761b2bc349f2bf3a5ae4a274227
ErikaAX08 <eamastalxochimitl@gmail.com>
2025-02-27 20:49:59 -0600

    Footer added

--- app/templates/footer.php
+++ app/templates/footer.php
@@ -1,3 +1,35 @@
-<footer>
-    <p>Copyright &copy; 2025</p>
+<footer class="footer">
+    <div class="company_info">
+        <h3 class="title">RIEAV</h3>
+        <p class="text">RIEAV es una empresa tecnológica fundada por Rodrigo, Iván, Erika, Axel y Valeria. Nuestro nombre refleja la unión de ideas y esfuerzos para crear soluciones innovadoras. Con creatividad y precisión, transformamos el futuro digital, conectando personas e ideas a través de la tecnología.</p>
+    </div>
+    <div class="menu_container">
+        <div>
+            <h3 class="title">Servicios</h3>
+            <ul class="menu_list">
+                <li class="text">Diseño Web</li>
+                <li class="text">Desarrollo Web</li>
+                <li class="text">Optimización SEO</li>
+                <li class="text">Blog</li>
+            </ul>
+        </div>
+        <div>
+            <h3 class="title">Enlaces Rápidos</h3>
+            <ul class="menu_list">
+                <li class="text">Inicio</li>
+                <li class="text">Servicio</li>
+                <li class="text">Testimonios</li>
+                <li class="text">Contacto</li>
+            </ul>
+        </div>
+        <div>
+            <h3 class="title">Ayuda y Soporte</h3>
+            <ul class="menu_list">
+                <li class="text">Centro de Soporte</li>
+                <li class="text">Chat en Vivo</li>
+                <li class="text">Preguntas Frecuentes</li>
+                <li class="text">Términos & Condiciones</li>
+            </ul>
+        </div>
+    </div>
 </footer>
